69792a1a8467df5cfa150cd89684f44b66bdf6c6
kaiiiz <ukaizheng@gmail.com>
2021-02-09 19:27:47 +0800

    Nested menu for desktop

--- assets/css/tailwind.config.js
+++ assets/css/tailwind.config.js
@@ -2,13 +2,17 @@ module.exports = {
     theme: {
         extend: {},
         fill: theme => ({
-            'gray-500': theme('colors.gray.500')
+            'gray-400': theme('colors.gray.400'),
+            'gray-500': theme('colors.gray.500'),
+            'gray-600': theme('colors.gray.600'),
+            'gray-800': theme('colors.gray.800')
         })
     },
     variants: {
         extend: {
             margin: ['first', 'last'],
             fill: ['hover'],
+            borderWidth: ['hover'],
         }
     },
     plugins: []
