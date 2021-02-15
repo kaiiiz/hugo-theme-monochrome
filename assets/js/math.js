4217ea62edbdecda531994db41fe0be8c3217a08
kaiiiz <ukaizheng@gmail.com>
2021-02-15 17:45:24 +0800

    Integrate mathjax

---
+++ assets/js/math.js
@@ -0,0 +1,11 @@
+window.MathJax = {
+    tex: {
+        inlineMath: [['$', '$']],
+        displayMath: [['$$', '$$']],
+        processEscapes: true,
+        processEnvironments: true
+    },
+    options: {
+        skipHtmlTags: ['script', 'noscript', 'style', 'textarea', 'pre']
+    }
+};
