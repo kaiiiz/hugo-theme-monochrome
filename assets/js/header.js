b878564cc11f043743b01d6b1af51f33251e51c1
kaiiiz <ukaizheng@gmail.com>
2022-03-29 04:22:20 +0800

    Basic sidebar support

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,14 +1,35 @@
-window.addEventListener("DOMContentLoaded", function () {
-    // const navbar_sm = document.getElementById("navbar_sm");
-    // const navbar_btn = document.getElementById('navbar_btn');
+function hide_canvas() {
+    const sidebar_canvas_overlay = document.getElementById("sidebar_canvas_overlay");
+    sidebar_canvas_overlay.classList.add("hidden");
+}
 
-    // navbar_btn.addEventListener('click', function () {
-    //     if (navbar_sm.classList.contains("hidden")) {
-    //         navbar_sm.classList.remove("hidden");
-    //     } else {
-    //         navbar_sm.classList.add("hidden");
-    //     }
-    // });
+function show_canvas() {
+    const sidebar_canvas_overlay = document.getElementById("sidebar_canvas_overlay");
+    sidebar_canvas_overlay.classList.remove("hidden");
+}
+
+function hide_sidebar() {
+    const sidebar = document.getElementById("sidebar");
+    sidebar.classList.add("close");
+}
+
+function show_sidebar() {
+    const sidebar = document.getElementById("sidebar");
+    sidebar.classList.remove("close");
+}
+
+window.addEventListener("DOMContentLoaded", function () {
+    const sidebar_btn = document.getElementById("sidebar_btn");
+    sidebar_btn.addEventListener("click", function () {
+        show_canvas();
+        show_sidebar();
+    });
+
+    const sidebar_canvas_overlay = document.getElementById("sidebar_canvas_overlay");
+    sidebar_canvas_overlay.addEventListener("click", function () {
+        hide_canvas();
+        hide_sidebar();
+    });
 
     const dark_mode_btn = document.getElementById("dark_mode_btn");
     const light_mode_btn = document.getElementById("light_mode_btn");
