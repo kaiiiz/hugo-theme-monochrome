4f9855183fed93a1d49b1c7c56de34754043aabb
kaiiiz <ukaizheng@gmail.com>
2023-07-23 16:45:03 +0800

    Upgrade prism to v1.29.0

--- assets/js/prism-autoloader.js
+++ assets/js/prism-autoloader.js
@@ -1,1 +1,1 @@
-Prism.plugins.autoloader.languages_path = 'https://cdnjs.cloudflare.com/ajax/libs/prism/1.27.0/components/';
+Prism.plugins.autoloader.languages_path = 'https://cdnjs.cloudflare.com/ajax/libs/prism/1.29.0/components/';
