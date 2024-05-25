f0129697bd9add5d259e69d24948db2d506d7a87
kaiiiz <kaiiiz@users.noreply.github.com>
2024-05-25 19:04:38 +0000

    deploy: 4b9a1f38c1e51d5840653447369351f15e87529a

--- js/math.d7efde37b2eb8879651e1f4489bcd4d8203b8c2bf8ca12c9e1b8cd11bfd6395b172f4999fff43ce0d047889a2bdb71ee74aebbae5327590192d1144e790fcd7b.js
+++
@@ -1,1 +0,0 @@
-(()=>{window.MathJax={tex:{inlineMath:[["$","$"]],displayMath:[["$$","$$"]],processEscapes:!0,processEnvironments:!0},options:{skipHtmlTags:["script","noscript","style","textarea","pre"]}};})();
