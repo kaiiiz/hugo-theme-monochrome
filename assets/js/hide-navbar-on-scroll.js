604efcc80bbb0d17e79da452343a490b08e2ba26
kaiiiz <ukaizheng@gmail.com>
2025-07-06 19:36:54 +0800

    feat: show navbar on hover

--- assets/js/hide-navbar-on-scroll.js
+++ assets/js/hide-navbar-on-scroll.js
@@ -1,8 +1,30 @@
-let lastScrollY = window.scrollY;
 const header = document.getElementsByTagName("header")[0];
 const threshold = 100;
 
-window.addEventListener("scroll", () => {
+let lastScrollY = window.scrollY;
+let isScrollHide = false;
+let headerHeight = (header !== undefined) ? header.offsetHeight : 0;
+
+function updateNavbarVisibility() {
+    if (isMouseNearTop || !isScrollHide) {
+        header.classList.remove('hide');
+    } else {
+        header.classList.add('hide');
+    }
+}
+
+function throttle(fn, wait) {
+  let lastTime = 0;
+  return function (...args) {
+    const now = Date.now();
+    if (now - lastTime >= wait) {
+      lastTime = now;
+      fn.apply(this, args);
+    }
+  };
+}
+
+window.addEventListener("scroll", throttle(() => {
     if (!header) {
         return;
     }
@@ -14,10 +36,21 @@ window.addEventListener("scroll", () => {
     }
 
     if (currentScrollY > lastScrollY && currentScrollY > 100) {
-        header.classList.add("hide");
+        isScrollHide = true;
     } else {
-        header.classList.remove("hide");
+        isScrollHide = false;
     }
 
     lastScrollY = currentScrollY;
+    updateNavbarVisibility();
+}, 50));
+
+window.addEventListener('resize', () => {
+    headerHeight = header.offsetHeight;
+    console.log("Resizing window, updating header height", headerHeight);
 });
+
+window.addEventListener('mousemove', throttle((e) => {
+  isMouseNearTop = e.clientY <= headerHeight;
+  updateNavbarVisibility();
+}, 50));
